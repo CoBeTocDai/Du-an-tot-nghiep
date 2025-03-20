005c0a8e5e140c73716c998e3aa678e7c80b3d2a
Khanhhh-20007 <huynhseng40@gmail.com>
2025-03-20 16:36:41 +0700

    Test Git

---
+++ TestGithu/src/main/java/com/edusys/testgithu/TestGithu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edusys.testgithu;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TestGithu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
